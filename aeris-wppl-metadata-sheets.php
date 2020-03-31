72b6f62da6d010d9812cf171c5452baa03453db2
Pierre Vert <pierr65@gmail.com>
2020-03-31 15:38:25 +0200

    tag ready

--- aeris-wppl-metadata-sheets.php
+++ aeris-wppl-metadata-sheets.php
@@ -6,7 +6,7 @@
 * Domain Path: /languages
 * Description: Manage AERIS metadatas sheets
 * Author: Pierre VERT
-* Version: 1.1.1
+* Version: 1.1.2
 * GitHub Plugin URI: aeris-data/aeris-wppl-metadata-sheets
 * GitHub Branch:     master
 */
