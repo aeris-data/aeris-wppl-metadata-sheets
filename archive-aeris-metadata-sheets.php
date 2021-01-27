9fe7a2f669ccd51088ff0c641e1600d9df1a9e19
Pierre VERT <vert@obs-mip.fr>
2021-01-27 22:28:10 +0100

    change scripts js

--- archive-aeris-metadata-sheets.php
+++ archive-aeris-metadata-sheets.php
@@ -59,9 +59,10 @@ $affichage_portfolio = get_field('sedoo_affichage_en_portfolio', $term);
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             	<section class="wrapper-content">
 					<!-- call webcomponent vueJs -->
-					<script type="text/javascript" component="aeris-data/aeris-commons-components-vjs@latest" src="https://rawcdn.githack.com/aeris-data/aeris-metadata-components-vjs/7468fa011600b3e430a3f0266b6b140c82a11e52/dist/aeris-metadata-components-vjs_0.9.5.js" ></script>
-                    <script type="text/javascript" component="aeris-data/aeris-metadata-components-vjs@latest" src="https://rawcdn.githack.com/aeris-data/aeris-metadata-components-vjs/7468fa011600b3e430a3f0266b6b140c82a11e52/dist/aeris-metadata-components-vjs_0.9.5.js" ></script>
-
+					<!-- <script type="text/javascript" component="aeris-data/aeris-commons-components-vjs@latest" src="https://rawcdn.githack.com/aeris-data/aeris-metadata-components-vjs/7468fa011600b3e430a3f0266b6b140c82a11e52/dist/aeris-metadata-components-vjs_0.9.5.js" ></script>
+                    <script type="text/javascript" component="aeris-data/aeris-metadata-components-vjs@latest" src="https://rawcdn.githack.com/aeris-data/aeris-metadata-components-vjs/7468fa011600b3e430a3f0266b6b140c82a11e52/dist/aeris-metadata-components-vjs_0.9.5.js" ></script> -->
+					<script type="text/javascript" src="https://services.sedoo.fr/cdn/js/aeris-commons-components-vjs_0.3.1.js" ></script>
+					<script type="text/javascript" src="https://services.sedoo.fr/cdn/js/aeris-metadata-components-vjs_0.9.6.js" ></script>
                     <aeris-metadata-synthesis service="https://sedoo.aeris-data.fr/catalogue/rest/metadatarecette/id/" identifier="<?php echo $uuid;?>" lang="<?php echo $lang;?>"/> 
 
 		        </section>				
